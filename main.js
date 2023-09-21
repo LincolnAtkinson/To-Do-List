9271df78f80f1226c5c922b116abe4d5f9964df8
fredrickatkinson <FREDRICKATKINSON@2088@STU.MTEC.EDU>
2023-09-21 10:12:11 -0600

    I created a new branch and then added some styling to the page

--- main.js
+++ main.js
@@ -47,23 +47,23 @@ showCurrentList(lists, currentListId);
 function render() {
     let listsHtml = '<ul class="list-group">';
     lists.forEach((list) => {
-        listsHtml += `<li class="list-group-item"><button class="list w-full bg-gray-300 h-14 text-2xl flex justify-center items-center">${list.name}</button></li>`;
+        listsHtml += `<li class="list-group-item"><button class="list w-full h-14">${list.name}</button></li>`;
     });
     listsHtml += '</ul>';
     console.log(listsHtml);
-    // Replace 'lists' with the ID of the element where you want to render the lists
+    
     document.getElementById('mainLists').innerHTML = listsHtml;
     document.getElementById('current-list-name').innerText = currentList.name;
 
     let todosHtml = '<ul class="list-group-flush">';
     currentList.todos.forEach((todo) => {
-        todosHtml += `<li class="list-group-item"><input type="checkbox">${todo.text}</li>`;
+        todosHtml += `<li class="current-list-todos"><input type="checkbox" id="check">${todo.text}</li>`;
     });
-    // Replace 'current-list-todos' with the ID of the element where you want to render todos
+    
     document.getElementById('current-list-todos').innerHTML = todosHtml;
 }
 
-// Call the render function to display lists and todos
+
 render();
 
 let b = document.querySelectorAll('button.list');
